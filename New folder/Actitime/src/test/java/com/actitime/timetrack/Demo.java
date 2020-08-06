826a5e0a8d64d1627dd1ff722e254a9af7282fc9
hi <hi@DESKTOP-RILUU24>
2020-08-06 20:11:25 +0530

    d

--- New folder/Actitime/src/test/java/com/actitime/timetrack/Demo.java
+++ New folder/Actitime/src/test/java/com/actitime/timetrack/Demo.java
@@ -10,10 +10,8 @@ public class Demo{
 		Reporter.log("Successfully running",true);
 		Reporter.log("Successfully running",true);
 		Reporter.log("Successfully running",true);
-		//System.out.println("ok na bro");
+		
 	}
 }
 
 
-//anthe inka ,..k na how change and check once show bro..this the the line we added na ...now it is not thr in tht..see now it will com
-// it is not thr na..now i'll do see'
