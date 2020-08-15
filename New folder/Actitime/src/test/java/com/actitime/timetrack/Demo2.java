8e09b43ea034125cdf9f4be161c9ad63488a23eb
hi <hi@DESKTOP-RILUU24>
2020-08-15 09:24:07 +0530

    ABC

---
+++ New folder/Actitime/src/test/java/com/actitime/timetrack/Demo2.java
@@ -0,0 +1,18 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo2 {
+
+		@Test
+		public void runTest()
+		{
+			Reporter.log("Successfully running",true);
+			Reporter.log("Hi i am Seven Hills are able to see me in jenkins???",true);
+			Reporter.log("Hello Edukondalu",true);
+			
+		}
+	}
+
+
